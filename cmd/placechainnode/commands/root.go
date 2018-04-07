abc82fadf92409bd68511749af9af3ec3f81ee05
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2018-04-07 13:12:43 +0200

    gofmt -s -w

--- cmd/placechainnode/commands/root.go
+++ cmd/placechainnode/commands/root.go
@@ -1,41 +1,41 @@
 package commands
 
 import (
-    "os"
+	"os"
 
-    "github.com/spf13/cobra"
-    "github.com/spf13/viper"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
-    "github.com/tendermint/tmlibs/cli"
-    tmflags "github.com/tendermint/tmlibs/cli/flags"
-    "github.com/tendermint/tmlibs/log"
+	"github.com/tendermint/tmlibs/cli"
+	tmflags "github.com/tendermint/tmlibs/cli/flags"
+	"github.com/tendermint/tmlibs/log"
 )
 
 const (
-    defaultLogLevel = "error"
-    FlagLogLevel    = "log_level"
+	defaultLogLevel = "error"
+	FlagLogLevel    = "log_level"
 )
 
 var (
-    logger = log.NewTMLogger(log.NewSyncWriter(os.Stdout)).With("module", "main")
+	logger = log.NewTMLogger(log.NewSyncWriter(os.Stdout)).With("module", "main")
 )
 
 var RootCmd = &cobra.Command{
-    Use:   "Place chain",
-    Short: "Draw beautiful things using a tendermint blockchain.",
-    PersistentPreRunE: func(cmd *cobra.Command, args []string) (err error) {
-        level := viper.GetString(FlagLogLevel)
-        logger, err = tmflags.ParseLogLevel(level, logger, defaultLogLevel)
-        if err != nil {
-            return err
-        }
-        if viper.GetBool(cli.TraceFlag) {
-            logger = log.NewTracingLogger(logger)
-        }
-        return nil
-    },
+	Use:   "Place chain",
+	Short: "Draw beautiful things using a tendermint blockchain.",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) (err error) {
+		level := viper.GetString(FlagLogLevel)
+		logger, err = tmflags.ParseLogLevel(level, logger, defaultLogLevel)
+		if err != nil {
+			return err
+		}
+		if viper.GetBool(cli.TraceFlag) {
+			logger = log.NewTracingLogger(logger)
+		}
+		return nil
+	},
 }
 
 func init() {
-    RootCmd.PersistentFlags().String(FlagLogLevel, defaultLogLevel, "Log level")
+	RootCmd.PersistentFlags().String(FlagLogLevel, defaultLogLevel, "Log level")
 }
--- cmd/placechainnode/main.go
+++ cmd/placechainnode/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"os"
 	commands "./commands"
 	"github.com/tendermint/tmlibs/cli"
+	"os"
 )
 
 func main() {
