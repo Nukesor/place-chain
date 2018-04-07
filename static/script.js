594e65728584456738f21a9ef5fe1f7cc60d8562
Benjamin Warnke <4bwarnke@informatik.uni-hamburg.de>
2018-04-07 14:46:00 +0200

    adding undefined color

--- static/script.js
+++ static/script.js
@@ -3,6 +3,7 @@ var canvascolor = document.getElementById('place_chain_color_chooser');
 var colorindex = 0;
 var pixelsize = 20;
 var colors = [
+	{},
 	{r: 0, g: 0, b: 0},
 	{r: 0, g: 0, b: 255},
 	{r: 0, g: 255, b: 0},
@@ -27,17 +28,17 @@ function roundPos(pos) {
 }
 function setPixel(canvas, pos, color) {
 	var context = canvas.getContext('2d');
-	context.fillStyle = "rgb("+color.r+","+color.g+","+color.b+")";
+	context.fillStyle = "rgb(" + color.r + "," + color.g + "," + color.b + ")";
 	context.fillRect( pos.x, pos.y, pixelsize, pixelsize );
 }
 function refreshColorChooser(canvas) {
 	var context = canvas.getContext('2d');
 	for (j = 0; j < 2; j++) {
 		for (i = 0; i < 4; i++) {
-			color=colors[i+j*4];
-			if(i+j*4 == colorindex){
+			color = colors[i + j * 4 + 1];
+			if(i + j * 4 + 1 == colorindex) {
 				context.fillStyle = "grey"
-				context.fillRect(i * 100,j * 100, 100, 100);
+				context.fillRect(i * 100, j * 100, 100, 100);
 				context.fillStyle = "rgb(" + color.r + "," + color.g + "," + color.b + ")";
 				context.fillRect(i * 100 + 5, j * 100 + 5, 90, 90);
 			}else{
@@ -47,24 +48,38 @@ function refreshColorChooser(canvas) {
 		}
 	}
 }
-$("#place_chain_canvas").click(function(evt){
+$("#place_chain_canvas").click(function(evt) {
 	var pos = roundPos(getMousePos(canvas, evt));
 	pos.color = colorindex;
 	setPixel(canvas,pos,colors[colorindex]);
 	$.post("pixel",
 		pos,
-		function(data, status){
+		function(data, status) {
 			$("#statusconsole").html("status = " + status + ", data = " + data);
 		}
 	);
 });
-$("#place_chain_color_chooser").click(function(evt){
+$("#place_chain_color_chooser").click(function(evt) {
 		var mousePos = getMousePos(canvascolor, evt);
-		colorindex = ((mousePos.x / 100) | 0) + ((mousePos.y / 100) | 0) * 4;
+		colorindex = ((mousePos.x / 100) | 0) + ((mousePos.y / 100) | 0) * 4 + 1;
 		refreshColorChooser(canvascolor);
 });
-refreshColorChooser(canvascolor);
+$(function() {
+	refreshColorChooser(canvascolor);
+	$.get( "pixels", function(data) {
+		width = data.length;
+		if(width == 0) {
+			$("#statusconsole").html("error: requesting \"pixels\" returned zero width");
+			return;
+		}
+		height = data[0].length;
+		if(width == 0) {
+			$("#statusconsole").html("error: requesting \"pixels\" returned not square area");
+			return;
+		}
+	});
+	//TODO longpoll node : image changes
+	//TODO longpoll node : my coins change
+	//TODO longpoll node : my color change ? oder nicht ?
+});
 
-//TODO longpoll node : image changes
-//TODO longpoll node : my coins change
-//TODO longpoll node : my color change ? oder nicht ?
