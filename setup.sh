e107e14c45d6ad8d65b792910b8e399c8348da54
Rafael Epplee <raffomania@gmail.com>
2018-04-07 18:12:49 +0200

    actually update deps

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-# go get -u -v github.com/satori/go.uuid
-# go get -u -v github.com/tendermint/abci/cmd/abci-cli
-# go get -u -v github.com/tendermint/tendermint/cmd/tendermint
+go get -u -v github.com/satori/go.uuid
+go get -u -v github.com/tendermint/abci/cmd/abci-cli
+go get -u -v github.com/tendermint/tendermint/cmd/tendermint
 cd "$GOPATH/src/github.com/tendermint/tendermint" && git checkout v0.17.1
 ln -sf "$(pwd)/.tendermint.genesis.json" "$HOME/.tendermint/config/genesis.json"
 ln -sf "$(pwd)/pre-commit" ".git/hooks/pre-commit"
