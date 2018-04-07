f199e4a142e425452bf6c0989ce663e208faebc9
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2018-04-07 14:43:12 +0200

    enum marshalling. error on unknown colors.

--- types/color.go
+++ types/color.go
@@ -1,14 +1,68 @@
 package types
 
+import (
+    "bytes"
+    "encoding/json"
+    "strconv"
+    "errors"
+)
+
 type Color int
 
 const (
-	A Color = iota
-	B
-	C
-	D
-	E
-	F
-	G
-	H
+	White Color = iota
+	Blue
+	Green
+	Yellow
+	Black
+	Cyan
+	Pink
+	Orange
+    Red
 )
+
+var dataTypesId = map[Color]int{
+    White: 0,
+    Blue: 1,
+    Green: 2,
+    Yellow: 3,
+    Black: 4,
+    Cyan: 5,
+    Pink: 6,
+    Orange: 7,
+    Red: 8,
+}
+
+var dataTypesName = map[int]Color{
+    0: White,
+    1: Blue,
+    2: Green,
+    3: Yellow,
+    4: Black,
+    5: Cyan,
+    6: Pink,
+    7: Orange,
+    8: Red,
+}
+
+func (c *Color) MarshalJSON() ([]byte, error) {
+    buffer := bytes.NewBufferString(`"`)
+    val := strconv.Itoa(dataTypesId[*c])
+    buffer.WriteString(val)
+    buffer.WriteString(`"`)
+    return buffer.Bytes(), nil
+}
+
+func (c *Color) UnmarshalJSON(b []byte) error {
+    var s int
+    err := json.Unmarshal(b, &s)
+    if err != nil {
+        return err
+    }
+    col, ok := dataTypesName[s]
+    if !ok {
+        return errors.New("Unknown color")
+    }
+    *c = col
+    return nil
+}
