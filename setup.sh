f4968592b866d273a9250dcd520c9c32c83c92c0
Hans Ole Hatzel <hhatzel@gmail.com>
2018-04-08 08:23:07 +0000

    Update setup script to install tendermint
    Don't worry if you get errors, that is by design!

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
-#go get -u -v github.com/satori/go.uuid
-#go get -u -v github.com/tendermint/abci/cmd/abci-cli
-#go get -u -v github.com/tendermint/tendermint/cmd/tendermint
+go get -u -v github.com/satori/go.uuid
+go get -u -v github.com/tendermint/abci/cmd/abci-cli
+go get -u -v github.com/tendermint/tendermint/cmd/tendermint
+cd "$GOPATH/src/github.com/tendermint/tendermint/cmd/tendermint" && git checkout v0.17.1 \
+	&& go build && mv tendermint "$GOPATH/bin/"
+
+
 ln -sf "$(pwd)/.tendermint.genesis.json" "$HOME/.tendermint/config/genesis.json"
 ln -sf "$(pwd)/pre-commit" ".git/hooks/pre-commit"
 
-cd "$GOPATH/src/github.com/tendermint/tendermint" && git checkout v0.17.1
 
 echo "==== ✅  installation successful ===="
