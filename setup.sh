a559daf08b43e6a3281df414388e2b3a1e81ee08
Hans Ole Hatzel <hhatzel@gmail.com>
2018-04-08 11:20:10 +0000

    Disable empty blocks in config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 tendermint init
 sed -i "s/auth_enc\ =\ true/auth_enc\ =\ false/" $HOME/.tendermint/config/config.toml
+sed -i "s/create_empty_blocks\ =\ true/create_empty_blocks\ =\ false/" $HOME/.tendermint/config/config.toml
 ln -sf "$(pwd)/tendermint.genesis.json" "$HOME/.tendermint/config/genesis.json"
 ln -sf "$(pwd)/pre-commit" ".git/hooks/pre-commit"
 
