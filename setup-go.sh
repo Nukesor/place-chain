2bba353ce9c98ea5b738151efac107a358c09700
Hans Ole Hatzel <hhatzel@gmail.com>
2018-04-08 10:10:11 +0000

    Downgrade tendermint version

--- setup-go.sh
+++ setup-go.sh
@@ -3,5 +3,5 @@ go get -u -v github.com/satori/go.uuid
 go get -u -v github.com/tendermint/abci/cmd/abci-cli
 go get -u -v github.com/tendermint/tendermint/cmd/tendermint
 
-cd "$GOPATH/src/github.com/tendermint/tendermint/cmd/tendermint" && git checkout v0.17.1 \
+cd "$GOPATH/src/github.com/tendermint/tendermint/cmd/tendermint" && git checkout v0.16.0 \
 	&& go build && mv tendermint "$GOPATH/bin/"
