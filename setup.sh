b2edb4a9487a0172c11d1580bcfe8a3f042e575b
Hans Ole Hatzel <hhatzel@gmail.com>
2018-04-08 15:22:38 +0200

    Add arnes ip as seed

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 tendermint init
 sed -i "s/auth_enc\ =\ true/auth_enc\ =\ false/" $HOME/.tendermint/config/config.toml
 sed -i "s/create_empty_blocks\ =\ true/create_empty_blocks\ =\ false/" $HOME/.tendermint/config/config.toml
+sed -i "s/seeds\ =\ .*/seeds\ =\ \"176.9.9.254:46656\"/" $HOME/.tendermint/config/config.toml
 ln -sf "$(pwd)/tendermint.genesis.json" "$HOME/.tendermint/config/genesis.json"
 ln -sf "$(pwd)/pre-commit" ".git/hooks/pre-commit"
 
